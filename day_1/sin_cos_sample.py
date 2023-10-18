65511bcef69d0347f7541ccedb34779f0f1610e8
zaned897 <3dmena@gmail.com>
2023-10-17 23:41:51 -0600

    fix: plots added to assignament

--- day_1/sin_cos_sample.py
+++ day_1/sin_cos_sample.py
@@ -1,11 +1,25 @@
 """In this code we will explore a function $f(x,y) = xsin(y) + ycos(x)$"""
+#%%
 from numpy import cos, sin, pi, linspace, meshgrid
 from matplotlib import pyplot as plt
 
+# create the space
 x = linspace(
 	start=-2*pi,
 	stop=2*pi,
 	num=200
 )
 y = x
-x, y = meshgrid
+X, Y = meshgrid(x, y)
+
+# Define the function
+Z = X*sin(Y) + Y*cos(X)
+
+#%% Results
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, Z, cmap='viridis')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_title('$g(x,y)=x\sin(y) + y\cos(x)$')
+plt.show()
